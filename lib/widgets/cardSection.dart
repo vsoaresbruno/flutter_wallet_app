8c8bd9dc8a0c53cb48e0ba35cdcfbf6f050ab630
Bruno Soares <vsoares.bruno@gmail.com>
2020-10-20 21:52:46 -0300

    feat: creditcard section

---
+++ lib/widgets/cardSection.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:wallet_app/data.dart';
+import 'package:wallet_app/widgets/cardDetails.dart';
+
+class CardSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          alignment: Alignment.topLeft,
+          child: Text(
+            'Selected Card',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+              itemCount: 2,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, i) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+                  decoration: BoxDecoration(
+                      color: primaryColor,
+                      boxShadow: customShadow,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Stack(children: <Widget>[
+                    Positioned.fill(
+                      left: -300,
+                      top: -100,
+                      bottom: -100,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            boxShadow: customShadow,
+                            shape: BoxShape.circle,
+                            color: Colors.white38),
+                      ),
+                    ),
+                    Positioned.fill(
+                      top: 150,
+                      bottom: -200,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            boxShadow: customShadow,
+                            shape: BoxShape.circle,
+                            color: Colors.white24),
+                      ),
+                    ),
+                    CardDetails()
+                  ]),
+                );
+              }),
+        )
+      ],
+    );
+  }
+}
