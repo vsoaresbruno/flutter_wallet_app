722213c13efe36e5606b9318a9ad6cb40e220bd9
Bruno Soares <vsoares.bruno@gmail.com>
2020-10-20 21:51:39 -0300

    feat: added wallet header

---
+++ lib/widgets/header.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:wallet_app/data.dart';
+
+class WalletHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            "Bruno's Wallet",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+          Container(
+            height: 50,
+            width: 50,
+            decoration: BoxDecoration(
+                boxShadow: customShadow,
+                color: primaryColor,
+                shape: BoxShape.circle),
+            child: Stack(
+              children: <Widget>[
+                Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        boxShadow: customShadow,
+                        color: Colors.deepOrange,
+                        shape: BoxShape.circle),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    margin: EdgeInsets.all(6),
+                    decoration: BoxDecoration(
+                        boxShadow: customShadow,
+                        color: primaryColor,
+                        shape: BoxShape.circle),
+                  ),
+                ),
+                Center(child: Icon(Icons.notifications))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
